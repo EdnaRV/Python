246e87b31b5aa1e4e94c029e29abb5350b50c255
EdnaRV <39867370+EdnaRV@users.noreply.github.com>
2021-09-27 15:52:04 -0500

    Add files via upload

---
+++ mm.py
@@ -0,0 +1,10 @@
+﻿import mouse
+import time
+import pyautogui, sys
+while(True):
+   mouse.move(50,350,absolute=True,duration=0)
+   time.sleep(4)
+   pyautogui.click(button='right')
+   mouse.move(70,450,absolute=True,duration=0)
+   time.sleep(4)
+   pyautogui.click(button='right')
